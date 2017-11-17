27893dcc0468d222705770cba8f4734a5b6ff675
JTranie <julien.tranie@sciencespo.fr>
2017-11-17 16:51:40 +0100

    VueChartApprentissage

--- charts/bar.js
+++ charts/bar.js
@@ -3,12 +3,21 @@ Vue.component('bar-chart', {
   props: ['data', 'options'],
   mounted () {
     var data = this.data || {
-      labels: [2007,2008,2009,2010,2011,2012,2013,2014,2015],
+      labels: ["AB","BC","MB","NB","NL","NS","NT","ON","PE","QC","SK","YT","NONE Specified"],
       datasets: [
         {
           label: 'Rhinoceros tués chaque année par le braconnage en Afrique du Sud',
           backgroundColor: '#f87979',
-          data: [13, null,122,333,448,668,1004,1215,749],
+          data: [92,60,18,10,6,11,0,348,2,189,25,0,6],
+          datalabels: {
+            align: 'end',
+            anchor: 'end'
+          }
+        },
+        {
+          label: 'Rhinoceros tués chaque année par le braconnage en Afrique du Sud',
+          backgroundColor: '#31a354',
+          data: [123.32958,44.33104,16.91827,8.76505,1.550,4.120,0,223.69222,0,116.52037,17.04626,0,11.181],
           datalabels: {
             align: 'end',
             anchor: 'end'
---
+++ exercice.js
@@ -0,0 +1,20 @@
+var eleves = [
+	{
+		nom: "Tranie",
+		prenom: "Julien",
+		age: 21,
+		pointure: 42,
+		cheveux: "bruns",
+		chauve: false,
+		myope: true
+	},
+	{
+		nom: "Maier",
+		prenom: "Jan",
+		age: 25,
+		pointure: 47,
+		cheveux: "roux",
+		chauve: false,
+		myope: true
+	}
+]
